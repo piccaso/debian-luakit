1d972b8d1e5ec5d7fdf9bd6671907a2d42a8c91c
Clint Adams <schizo@debian.org>
2010-08-06 08:19:15 -0400

    use DESTDIR for man & docs

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ VERSION   ?= $(shell ./build-utils/getversion.sh)
 # Paths
 PREFIX     ?= /usr
 INSTALLDIR ?= ${DESTDIR}${PREFIX}
-MANPREFIX  ?= ${PREFIX}/share/man
-DOCDIR     ?= ${PREFIX}/share/luakit/docs
+MANPREFIX  ?= ${DESTDIR}${PREFIX}/share/man
+DOCDIR     ?= ${DESTDIR}${PREFIX}/share/luakit/docs
 
 # Generate includes and libs
 # liblua5.1-0-dev libgtk2.0-dev libwebkit-dev libxdg-basedir-dev gperf
